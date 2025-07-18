cf352f9460fef871411591d506a4797cde940dca
Luke Taylor <luke.taylor@bbc.co.uk>
2025-07-18 15:49:14 +0100

    add map

---
+++ 04-building-pipelines/map.ts
@@ -0,0 +1,18 @@
+import { Effect, pipe } from "effect";
+
+/*
+ * Map
+ * Transofrms the value inside an effect by applying a function to it
+ */
+
+
+const addTip = (amount: number) => amount + 1;
+
+const fetchTransactionAmount = Effect.promise(() => Promise.resolve(100))
+
+const finalAmount = pipe(
+  fetchTransactionAmount,
+  Effect.map(addTip)
+)
+
+Effect.runPromise(finalAmount).then(console.log)
