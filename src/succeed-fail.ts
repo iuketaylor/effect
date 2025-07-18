122fe234989f41908fd425bdd36d546253f75ad8
Luke Taylor <luke.taylor@bbc.co.uk>
2025-07-18 11:19:23 +0100

    formatting

--- src/succeed-fail.ts
+++ src/succeed-fail.ts
@@ -1,5 +1,16 @@
 import { Effect, Record } from "effect";
 
+/*
+ * Summary:
+   * Effect introduces dedicated constructors for creating effects that represent both success and failure
+   * Effect.succeed: Creates an effect that always succeeds with a given value
+   * Effect.fail: Creates an effect that represents an error that can be recovered from
+   * These constructors allow you to handle success and failure cases while leveraging the type system to track errors
+ * Opinion:
+   * This is a much better way of handling errors in a program
+   * Not really a fan of the verbosty of Effect.succeed
+ */
+
 interface User {
   readonly id: number;
   readonly name: string;
@@ -11,16 +22,16 @@ const getUser = (userId: number): Effect.Effect<User, Error> => {
     2: { id: 2, name: "Jane Smith" },
   };
 
-  const user = userDatabase[userId]
+  const user = userDatabase[userId];
   if (user) {
-    return Effect.succeed(user)
+    return Effect.succeed(user);
   } else {
-    return Effect.fail(new Error("User not found"))
+    return Effect.fail(new Error("User not found"));
   }
 };
 
-const successfulEffect = getUser(1)
-console.log(successfulEffect)
+const successfulEffect = getUser(1);
+console.log(successfulEffect);
 
-const errorEffect = getUser(4)
-console.log(errorEffect)
+const errorEffect = getUser(4);
+console.log(errorEffect);
